a3d8c4cb43476fb4bee9ea8548463576269baada
CarlosSlim <e.olguin89@yahoo.com.mx>
2019-01-27 18:04:26 -0600

    Subiendo primera versión index.html styles.css functions.js e imagenes

---
+++ js/functions.js
@@ -0,0 +1,34 @@
+function drawBorder(){
+  var text = document.getElementById("fb").style.border="1px solid white";
+}
+function noBorder(){
+  var text = document.getElementById("fb").style.border="";
+}
+
+function drawBorderTw(){
+  var text = document.getElementById("tw").style.border="1px solid black";
+}
+function noBorderTw(){
+  var text = document.getElementById("tw").style.border="";
+}
+
+function drawBorderGit(){
+  var text = document.getElementById("git").style.border="1px solid black";
+}
+function noBorderGit(){
+  var text = document.getElementById("git").style.border="";
+}
+
+function drawBorderInsta(){
+  var text = document.getElementById("insta").style.border="1px solid black";
+}
+function noBorderInsta(){
+  var text = document.getElementById("insta").style.border="";
+}
+
+function drawBorderB(){
+  var text = document.getElementById("boton_news").style.color="white";
+}
+function noBorderB(){
+  var text = document.getElementById("boton_news").style.color="black";
+}
